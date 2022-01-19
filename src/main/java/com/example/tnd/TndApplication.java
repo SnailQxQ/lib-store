a48e0a91a61fa1296f6a0a50bf49fa1b10654b2c
Turbine <turbine@gmail.com>
2022-01-19 13:44:26 +0800

    create Project

---
+++ src/main/java/com/example/tnd/TndApplication.java
@@ -0,0 +1,13 @@
+package com.example.tnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TndApplication.class, args);
+    }
+
+}
